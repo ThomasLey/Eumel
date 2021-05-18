81fd6682df82f2def3dcbcb972d2e23d05b701e4
Thomas Ley <thomas-ley@outlook.de>
2021-05-18 14:19:23 +0200

    clean up old solution

--- Add-Document.ps1
+++
@@ -1,9 +0,0 @@
-$endpoint = https://localhost:44380/document
-
-$get = Invoke-WebRequest -Uri $endpoint -UseBasicParsing
-Write-Information $get
-
-$file = "Add-Document.ps1"
-$upload = Invoke-WebRequest -uri "$endpoint/Upload" -Method Post -Infile $file
-
-Write-Information $upload
