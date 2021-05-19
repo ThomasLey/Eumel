5e108d1c9f6eb3021565ccf4942d4ea09f239376
Thomas Ley <thomas-ley@outlook.de>
2021-05-19 18:28:55 +0200

    cleanup.

---
+++ Eumel.Domse.Specflow/Drivers/GetAListOfDocumentsDriver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Eumel.Domse.BusinessLogic;
+using Eumel.Domse.Core;
+
+namespace Eumel.Domse.Specflow.Drivers
+{
+    internal class GetAListOfDocumentsDriver
+    {
+        public string StoragePath { get; set; }
+        public IStorageService StorageService { get; set; }
+        public IEnumerable<DocumentInformation> Documents { get; set; }
+
+        public IEnumerable<DocumentInformation> GetDocuments()
+        {
+            var ops = new DocumentOperator(new Lazy<IStorageService>(StorageService));
+            return ops.GetDocumentList();
+        }
+    }
+}
---
+++ Eumel.Domse.Specflow/Drivers/PowershellCmdletDriver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace Eumel.Domse.Specflow.Drivers
+{
+    internal class PowershellCmdletDriver
+    {
+        private readonly Dictionary<string, string> _parameter = new();
+        public Cmdlet Cmdlet { get; set; }
+
+        public void AddParameter(string key, string value)
+        {
+            _parameter.Add(key, value);
+        }
+
+        public void Execute()
+        {
+            foreach (var propertyInfo in typeof(Cmdlet).GetProperties().Where(x => CustomAttributeExtensions.GetCustomAttribute<ParameterAttribute>((MemberInfo) x) != null))
+            {
+                propertyInfo.SetValue(Cmdlet, _parameter[propertyInfo.Name]);
+            }
+
+            Result = Cmdlet.Invoke();
+        }
+
+        public IEnumerable Result { get; set; }
+    }
+}
